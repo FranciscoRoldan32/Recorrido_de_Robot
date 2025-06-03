83d6f7e3b9734eb35467640a05407ff968fda2e2
Alejandro <correa92.dev@gmail.com>
2025-06-03 13:40:12 -0300

    Se implementa logica y dto

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,14 +1,17 @@
 package controller;
 
+import java.util.Arrays;
+
+import model.Dto.ResultDto;
 import model.Services.MatrixService;
 import view.Matrix_View;
 
 public class Controller {
-	private MatrixService service;
+	private MatrixService matrixService;
 	private Matrix_View mat_View;
 	
 	public Controller () {
-		this.service = new MatrixService();
+
 		initAplication();
 	}
 
@@ -19,9 +22,33 @@ public class Controller {
 		
 		
 		//una vez q el user ingrese los archivos generar matriz
-		this.service.generateMatrix(5, 6);
-		this.service.printMatrix();
+		int[][] gridTemporal = MatrixService.generateMatrix(15, 16);//matriz temporal
 		
+		this.matrixService = new MatrixService(gridTemporal);
+		this.matrixService.printMatrix();
+		
+		ResultDto resultDto = this.matrixService.runAlgorithm();
+		
+		if (!this.matrixService.validateNumberOfStepsIsEven()) {
+            System.out.println("No se puede encontrar un camino válido: cantidad de pasos impar.");
+		} 
+
+        System.out.println("\n==== Resultados ====");
+        System.out.println("¿Camino válido encontrado?: " + resultDto.pathFound);
+        System.out.println("Tiempo sin poda: " + resultDto.timeWithoutPrune + " ms");
+        System.out.println("Tiempo con poda: " + resultDto.timeWithPrune + " ms");
+        System.out.println("Llamadas sin poda: " + resultDto.callsWithoutPrune);
+        System.out.println("Llamadas con poda: " + resultDto.callsWithPrune);
+
+        if (resultDto.pathFound) {
+            System.out.println("Camino válido:");
+            for (int[] pos : resultDto.validPath) {
+                System.out.print(Arrays.toString(pos) + " ");
+            }
+            System.out.println();
+        }
+        
+        this.matrixService.printMatrixWithPath(resultDto.validPath);
 
 //		mat_View.drawMatrix(buttons, n, m);
 
--- src/model/Services/MatrixService.java
+++ src/model/Services/MatrixService.java
@@ -1,34 +1,82 @@
 package model.Services;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+
+import model.Dto.ResultDto;
 
 public class MatrixService {
-	private int row, col;
+	private int rows, cols;
 	private int[][] matrix;
-
+	private List<int[]> bestPath = new ArrayList<>();
+	private boolean[][] visited;
+	private int totalCallsWithoutPrune = 0;
+	private int totalCallsWithPrune = 0;
+	
 	public MatrixService() {}
 
-	public int[][] generateMatrix(int rows, int cols) {
+	public MatrixService(int[][] matrix) {
+		this.matrix = matrix;
+		this.rows = matrix.length;
+		this.cols = matrix[0].length;
+		this.bestPath = new ArrayList<>();
+	}
+
+	public static int[][] generateMatrix(int rows, int cols) {
 		Random rand = new Random();
-		this.row = rows;
-		this.col = cols;
-		this.matrix = new int[row][col];
-		for (int i = 0; i < this.matrix.length; i++) {
-			for (int j = 0; j < this.matrix[0].length; j++) {
-				this.matrix[i][j] = rand.nextBoolean() ? 1 : -1;
+
+		int[][] grid = new int[rows][cols];
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				grid[i][j] = rand.nextBoolean() ? 1 : -1;
 			}
 		}
-		return this.matrix;
+		return grid;
 	}
 
 	public boolean validateNumberOfStepsIsEven() {
-		return (row + col - 1) % 2 == 0;
+		return (rows + cols - 1) % 2 == 0;
 	}
 
 	public int getNumberOfSteps() {
-		return row + col - 1;
+		return rows + cols - 1;
 	}
+	
+	public ResultDto runAlgorithm() {
+		
+        if (!validateNumberOfStepsIsEven()) {
+            return new ResultDto(false, 0, 0, 0, 0, Collections.emptyList());
+        }
+        
+        // Sin poda
+        totalCallsWithoutPrune = 0;
+        long start1 = System.currentTimeMillis();
+        boolean foundWithoutPrune = backtrack(0, 0, 0, new ArrayList<>(), false);
+        long end1 = System.currentTimeMillis();
+        long timeWithoutPrune = end1 - start1;
 
+        // Con poda
+        bestPath.clear();
+        totalCallsWithPrune = 0;
+        long start2 = System.currentTimeMillis();
+        boolean foundWithPrune = backtrack(0, 0, 0, new ArrayList<>(), true);
+        long end2 = System.currentTimeMillis();
+        long timeWithPrune = end2 - start2;
+        
+        return new ResultDto(
+                foundWithPrune || foundWithoutPrune,
+                timeWithoutPrune,
+                timeWithPrune,
+                totalCallsWithoutPrune,
+                totalCallsWithPrune,
+                new ArrayList<>(bestPath)
+            );
+	}
+	
 	public void printMatrix() {
 		for (int[] row : this.matrix) {
 			for (int val : row) {
@@ -36,5 +84,68 @@ public class MatrixService {
 			}
 			System.out.println();
 		}
+	}
+	
+	public void printMatrixWithPath(List<int[]> path) {
+	    Set<String> pathSet = new HashSet<>();
+	    for (int[] pos : path) {
+	        pathSet.add(pos[0] + "," + pos[1]);
+	    }
+
+	    for (int i = 0; i < matrix.length; i++) {
+	        for (int j = 0; j < matrix[0].length; j++) {
+	            String key = i + "," + j;
+	            String cellStr;
+
+	            if (pathSet.contains(key)) {
+	                cellStr = String.format("[%d]", matrix[i][j]);
+	            } else {
+	                cellStr = String.format(" %d ", matrix[i][j]);
+	            }
+
+	            System.out.printf("%4s", cellStr);
+	        }
+	        System.out.println();
+	    }
+	}
+
+
+	private boolean backtrack(int i, int j, int sum, List<int[]> path, boolean usePrune) {
+		
+		if (usePrune)
+			totalCallsWithPrune++;
+		else
+			totalCallsWithoutPrune++;
+
+		if (i >= this.rows || j >= this.cols)
+			return false;
+
+		sum += this.matrix[i][j];
+		path.add(new int[] { i, j });
+
+		// Poda inteligente
+		if (usePrune) {
+			int stepsLeft = (rows - 1 - i) + (cols - 1 - j);
+
+			if (Math.abs(sum) > stepsLeft) {
+				path.remove(path.size() - 1);
+				return false;
+			}
+		}
+
+		//destino
+		if (i == rows - 1 && j == cols - 1) {
+			if (sum == 0) {
+				bestPath = new ArrayList<>(path);
+				return true;
+			}
+			path.remove(path.size() - 1);
+			return false;
+		}
+
+		boolean found = backtrack(i + 1, j, sum, path, usePrune) || backtrack(i, j + 1, sum, path, usePrune);
+
+		path.remove(path.size() - 1); // backtrack
+		return found;
 	}
 }
